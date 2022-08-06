08d292e936311d9b84d5e77804e1707cbaa7cf7b
Norty <nortyhv@gmail.com>
2022-08-06 23:06:12 +0300

    12

--- src/main/java/entity/Wallet.java
+++ src/main/java/entity/Wallet.java
@@ -1,4 +1,48 @@
 package entity;
 
+import data.DataSource;
+import model.CurrencyPair;
+import model.Symbol;
+import services.IOService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wallet {
+    IOService ioService = new IOService();
+    DataSource dataSource = new DataSource();
+
+    List<String> hystory;
+
+    int currentState ;
+
+    String currency;
+
+    public Wallet(List<String> hystory, int currentState, String currency) {
+        this.hystory = new ArrayList<>();
+        this.currentState = currentState;
+        this.currency = currency;
+    }
+
+    public void exchange(String Currency) throws IOException {
+        String str = ioService.read();
+
+        List <Symbol> list = dataSource.getPairBySymbol();
+        for (int i = 0; i < list.size(); i++) {
+            if
+        }
+
+
+    }
+    public void add(){
+
+    }
+    public void cashIssue(){
+
+    }
+    public void getHystory(){
+
+    }
+
 }
