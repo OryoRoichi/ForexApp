68001c8cf19593424aafb825698b1cbe2e4d2fb7
NortyHV <nortyhv@gmail.com>
2022-08-08 09:02:19 +0300

    Method exchange

--- src/main/java/ForexApp.java
+++ src/main/java/ForexApp.java
@@ -1,12 +1,53 @@
 import data.DataSource;
 import model.Symbol;
 
+import data.DataSource;
+import entity.Wallet;
+import model.CurrencyPair;
+import model.Symbol;
+import services.IOService;
+
+import java.io.IOException;
+import java.util.List;
+
 public class ForexApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         DataSource dataSource = new DataSource();
-        Symbol s = new Symbol();
-        s.setSymbol("EUR/USD");
-        System.out.println(dataSource.getSymbolsList());
+        //Symbol s = new Symbol();
+        //s.setSymbol("EUR/USD");
+        //System.out.println(dataSource.getPairById(16));
+        // System.out.println(dataSource.getSymbolsList());
+        //List<Symbol> list = dataSource.getSymbolsList();
+        //for(int i = 0 ; i < list.size();i++ ){
+        //if(list.get(i).equals(s)){
+        // System.out.println(list.get(i));
+        //}
+        //}
+
+        /*List<CurrencyPair> currencyPairs = dataSource.getPairBySymbol(list.get(0));
+        for (CurrencyPair pair: currencyPairs) {
+            System.out.println(pair);
+        }*/
+
+        Wallet wallet = new Wallet(10000, "usd");
+        System.out.println(wallet.toString());
+        wallet.exchange("usd", "eur", 1000);
+        System.out.println(wallet.toString());
+
+        IOService ioService = new IOService();
+        System.out.println("1 валюта");
+
+        String v1 = ioService.read();
+        System.out.println("1 валюта");
+
+        String v2 = ioService.read();
+
+        String sum = ioService.read();
+        wallet.exchange(v1, v2, Integer.parseInt(sum));
+        System.out.println(wallet.toString());
+
+        wallet.printHistory();
+
     }
 }
--- src/main/java/entity/Wallet.java
+++ src/main/java/entity/Wallet.java
@@ -1,38 +1,71 @@
-package entity;
+
+        package entity;
 
 import data.DataSource;
 import model.CurrencyPair;
 import model.Symbol;
-import services.IOService;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Wallet {
-    IOService ioService = new IOService();
-    DataSource dataSource = new DataSource();
 
     List<String> hystory;
 
-    int currentState ;
+    Map<String, Integer> valuts = new HashMap<>();
+    List<Symbol> list;
+    DataSource dataSource;
 
-    String currency;
-
-    public Wallet(List<String> hystory, int currentState, String currency) {
+    public Wallet(int currentState, String currency) {
         this.hystory = new ArrayList<>();
-        this.currentState = currentState;
-        this.currency = currency;
+        valuts.put(currency.toUpperCase(), currentState);
+
+        dataSource = new DataSource();
+        list = dataSource.getSymbolsList();
     }
 
-    public void exchange(String Currency) throws IOException {
-        String str = ioService.read();
 
-        List <Symbol> list = dataSource.getPairBySymbol();
-        for (int i = 0; i < list.size(); i++) {
-            if
+    //метод обмен валют параметры
+    //srcCurrency = валюта которую хотим поменять
+    //валюта на оторую хотим поменять
+    //sum сумма, которую хотим поменять
+    public void exchange(String srcCurrency, String targetCurrency, int sum){
+        srcCurrency = srcCurrency.toUpperCase();     //Возводим строку в верхний регитр
+        targetCurrency = targetCurrency.toUpperCase();
+        String pair1 = srcCurrency+"/"+targetCurrency;  //Создаём пару
+        String pair2 = targetCurrency+"/"+srcCurrency;
+        Symbol found = null;
+        for (Symbol symbol: list) {                   //ищем в списке нужную пару символов, которую будем использовать при обмене
+            if (symbol.getSymbol().equals(pair1) || symbol.getSymbol().equals(pair2)) {
+                found = symbol;
+                break;
+            }
         }
+        if (found == null) {
+            System.out.println("Валюта(ы) не найдена");
+        } else {
+            int sumInWallet = valuts.get(srcCurrency);  //проверяем хватает ли денег в кашельке
+            if (sumInWallet<sum) {
+                System.out.println("Недостаточно средств");
+            } else {
+                List<CurrencyPair> currencyPairs = dataSource.getPairBySymbol(found);
+                if (currencyPairs != null && currencyPairs.size()>0) {
+                    int newSum ;  // Сумм после конвертации
+                    if (found.getSymbol().equals(pair1)) {
+                        newSum = (int) (currencyPairs.get(0).getPrice() * sum);
+                    } else {
+                        newSum = (int) (sum / currencyPairs.get(0).getPrice());
+                    }
+                    valuts.put(srcCurrency, valuts.get(srcCurrency)-sum);
+                    //Добавляем сумму в кошелек,в map list, если такого ключа(валюты) не было создаём
+                    int tSum = valuts.containsKey(targetCurrency) ? valuts.get(targetCurrency) : 0;
 
+                    hystory.add(sum+"-> "+found.getSymbol()+" -> "+newSum);
+                } else {
+                    System.out.println("что-то не так");
+                }
+
+            }
+        }
 
     }
     public void add(){
@@ -45,4 +78,17 @@ public class Wallet {
 
     }
 
+    public void printHistory() {
+        for (String s: hystory) {
+            System.out.println(s);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Wallet{" +
+                "valuts=" + valuts +
+                '}';
+    }
 }
+
